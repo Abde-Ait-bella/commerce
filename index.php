ac7b34adc13f9b3dec76bea4d196f3d11d483f86
boukadia <mustapha.boukadia97@gmail.com>
2025-02-06 09:02:07 +0100

    feat: realiser routing

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
+// require './vendor/autoload.php';
+
+// use Src\Controllers\ProductController;
+
+
+require './src/routes/routes.php';
+include_once($_SERVER['DOCUMENT_ROOT'] .'./commerce/src/controller/ProductController.php');
+
+
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-$uriClean = str_replace('/commerce/', '', strtok($uri, "?"));
+$uriclean = str_replace('/commerce', '', $uri);
 
+$url = parse_url($uriclean, PHP_URL_PATH);
 
+$routes = new Routes();
+$routes->Dispatch($method, $url);
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,12 +1,30 @@
 <?php
 
+// use Src\Controllers\ProductController;
 
+include_once($_SERVER['DOCUMENT_ROOT'] .'./commerce/src/controller/ProductController.php');
 
 class Routes
 {
-    private array $Routes = [
+    private array $routes = [
+        "POST" => [
+            // "/signup" => [UserController::class, "register"],
+            // "/signin" => [UserController::class, "login"],
+            // "/addProduct" => [ProductController::class, "Create"],
+            // "/updateProduct" => [ProductController::class, "Update"],
+            // "/addOrder" => [OrderContreller::class, "Create"],
+        ],
         "GET" => [
-            '/' => [BokadiaController::class , 'index'],
+            '/' => [ProductController::class, 'index'],
+            '/admine' => [ProductController::class, 'productAdmin'],
         ]
-    ]
+    ];
+
+    public function dispatch($method, $uri){
+        if ($this->routes[$method][$uri]) {
+            [$class, $action] = $this->routes[$method][$uri];
+            $controller = new $class();
+            $controller->$action();
+        }
+    }
 }
